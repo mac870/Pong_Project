57b38304a03881cd2b338b5e65327107f9ce96dd
mac870 <157775260+mac870@users.noreply.github.com>
2024-03-14 13:04:20 -0500

    Add files via upload

---
+++ Pong.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pong;
+import javax.swing.*;
+import java.awt.Color;
+import javax.swing.JFrame;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+public class Pong extends JFrame {
+    
+
+    public static void main(String[] args) {
+   
+    }
+    
+}
